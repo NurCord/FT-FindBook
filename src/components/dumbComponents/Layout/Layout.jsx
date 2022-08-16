eac28e084655c2d5d66951fbfcbf133b872cb3ed
NurCord <nunyypin@gmail.com>
2022-08-15 22:30:22 -0300

    fix(src/LayoutAdmin, NavBar, Footer) arregle el nav y agrege el footer

--- src/components/dumbComponents/Layout/Layout.jsx
+++ src/components/dumbComponents/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NavBar from '../NavBar/NavBar'
-
+import Footer from '../Footer/Footer'
 
 function Layout() {
   return (
     <div>
         <NavBar/>
         <Outlet/>
+        <Footer/>
     </div>
   )
 }
