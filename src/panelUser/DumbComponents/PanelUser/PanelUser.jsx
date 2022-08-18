5f1e12d92aef35ba6713908c26da3c1b0821896b
Heider <heiderlopez123@gmail.com>
2022-08-18 17:05:34 -0500

    responsive(componente panel admin usuer ya es responsive)

--- src/panelUser/DumbComponents/PanelUser/PanelUser.jsx
+++ src/panelUser/DumbComponents/PanelUser/PanelUser.jsx
@@ -3,7 +3,9 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import NavBarHome from '../NavBarHome/NavBarHome'
-import {userDetailPanel , booksPanel} from '../../../redux/actions/actions'
+import { userDetailPanel, booksPanel } from '../../../redux/actions/actions'
+import clsx from 'clsx'
+
 function PanelUser({ SetHomeUser }) {
     const role = useSelector(state => state.root.role)
     const dispatch = useDispatch()
@@ -14,7 +16,9 @@ function PanelUser({ SetHomeUser }) {
     }, [])
     if (role !== "loading") {
         return (
-            <div className='w-full h-screen'>
+            <div className={clsx(
+                'mobile:h-screen',
+                'desktop:w-full desktop:h-screen')}>
                 <div className='grid w-full h-auto grid-cols-6'>
                     <div className='h-auto col-span-1 bg-greyBlack-100'>
                         <NavBarHome SetHomeUser={SetHomeUser} />
