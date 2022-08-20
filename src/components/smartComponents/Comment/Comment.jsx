c78ee7f2487aeca2d9989648448f0fc488263304
catalancesar <cesar-catalan@hotmail.es>
2022-08-20 14:04:41 -0500

    Fix (Comment.jsx) CesarBranch comment function fixed

--- src/components/smartComponents/Comment/Comment.jsx
+++ src/components/smartComponents/Comment/Comment.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React from 'react';
 
-export default function Comment({ timestamp, comentario }) {
+export default function Comment({ comentario, timestamp, usuario }) {
    
-
+   let options = { 
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZone: 'UTC' };
+   let postDate = new Date(timestamp).toLocaleDateString('es-ES', options)
    return (
       <div>
-         <div className='flex items-center justify-center min-h-screen'>  <div className="rounded-xl border p-5 shadow-md w-9/12 bg-white">
-            <div className="flex w-full items-center justify-between border-b pb-3">
-               <div className="flex items-center space-x-3">
-                  <div className="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://static.vecteezy.com/system/resources/previews/002/896/415/non_2x/books-illustration-cartoon-books-books-vector.jpg')]"></div>
-                  <div className="text-lg font-bold text-slate-700">Usuario</div>
-               </div>
+         <div className='flex items-center justify-center mt-2'>  
+            <div className="rounded-xl border p-5 shadow-md w-full bg-white">
+               <div className="flex w-full items-center justify-between border-b pb-3">
+                  <div className="flex items-center space-x-3">                   
+                     <img className="h-8 w-8 rounded-full" src={`${usuario.url}`}/>
+                     <div className="text-lg font-bold text-slate-700">{`${usuario.name} ${usuario.lastname}`}</div>
+                  </div>
                <div className="flex items-center space-x-8">
-                  {/* <div className="text-xs text-neutral-500">{timestamp}</div> */}
+                  <div className="text-xs text-neutral-500">{postDate}</div>
                </div>
             </div>
 
             <div className="mt-4 mb-6">
-               <div className="mb-3 text-xl font-bold">Un usuario comentó:</div>
+               <div className="mb-3 text-xl font-bold">Comentó:</div>
                <h1 className="text-sm text-neutral-600">{comentario}</h1>
             </div>
          </div>
