fd0de0395f8184d6f69a0097cea43369faaac592
rmauriciocastilla <rmauriciocastilla@gmail.com>
2022-08-20 21:40:35 -0500

    Fixing Client sales orders(Show sales)

--- src/panelUser/DumbComponents/UserOrders/UserOrders.jsx
+++ src/panelUser/DumbComponents/UserOrders/UserOrders.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { getUserOrders } from '../../../redux/actions/actionsShop'
+import { getUserOrders, getUserSales } from '../../../redux/actions/actionsShop'
 import OrderCards from './OrderCards'
 import Swal from "sweetalert2";
 import { Link } from 'react-router-dom';
@@ -12,12 +12,15 @@ export default function UserOrders() {
   const dispatch = useDispatch()
   const navigate = useNavigate();
   const orderList = useSelector(state => state.shop.orderList)
+  const salesList = useSelector(state => state.shop.salesList)
   useEffect(() => {
     dispatch(getUserOrders())
+    dispatch(getUserSales())
   }, [])
 
   if(role === "user"){
     return (
+    <div className='flex flex-wrap'>
       <div>
       <div className="h-screen bg-gray-300">
         <div className="py-12">
@@ -43,6 +46,32 @@ export default function UserOrders() {
         </div>
       </div>
     </div>
+          <div>
+          <div className="h-screen bg-gray-300">
+            <div className="py-12">
+              <div className="max-w-md mx-auto bg-gray-100 shadow-lg rounded-lg  md:max-w-5xl">
+                <div className="md:flex ">
+                  <div className="w-full p-4 px-5 py-5">
+                    <div className="md:grid md:grid-cols-3 gap-2 ">
+                      <div className="col-span-2 p-5">
+                        <h1 className="text-xl font-medium ">Historial de ventas</h1>
+                        { salesList && salesList.length ?
+                        <OrderCards orderUser={salesList}/> :
+                        <span>No tienes historial de venta</span>}
+                        <div className="flex justify-between items-center mt-6 pt-6 border-t">
+                          <div className="flex items-center">
+                            <i className="fa fa-arrow-left text-sm pr-2"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   );
   }else{
     Swal.fire({
