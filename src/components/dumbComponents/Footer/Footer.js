d70343bf3f801064fadb9a8baee207e02eb05ed3
NurCord <nunyypin@gmail.com>
2022-08-24 00:59:46 -0300

    feat(scr/footer / bot) NuriaBranch css

--- src/components/dumbComponents/Footer/Footer.js
+++ src/components/dumbComponents/Footer/Footer.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import clsx from 'clsx'
+import logo from '../../../assets/FindBookLogo.png'
 export default function Footer() {
     return (
         <div className={clsx(
             'mobile:text-sm',
-            'w-full desktop:text-base desktop:h-24 grid grid-rows-3'
+            'w-full desktop:text-base desktop:h-44 grid grid-rows-3'
         )}>
-            <div className='w-full row-span-2 bg-greyBlack-200'>
+        <div  className='grid grid-cols-2 justify-items-start content-center row-span-2 bg-[#c5b081]'>
+            <div className='w-full'>
                 <Link to={'/contacto'}>
-                    <div className='flex items-center justify-center h-full'>
-                        <svg xmlns="http://www.w3.org/2000/svg" className={clsx(
-                            'mobile:w-5 mobile:h-5',
-                            "desktop:w-6 desktop:h-6 mx-2"
-                            )} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                        </svg>
-                        <h1>Contacto</h1>
+                    <div className='flex flex-col items-center justify-center w-full h-full'>
+                        <img src={logo} className='z-10 h-16' alt='Not found'/>
+                        <div className='flex'>
+                            <svg xmlns="http://www.w3.org/2000/svg" className={clsx(
+                                'mobile:w-5 mobile:h-5',
+                                "desktop:w-6 desktop:h-6 mx-2"
+                                )} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                            </svg>
+                            <h1>Contacto</h1>
+                        </div>
                     </div>
                 </Link>
             </div>
-            <div className='grid content-center w-full justify-items-center bg-greyBlack-300'>
+            <div className='grid content-center w-full h-full'>
+                <h1 className='text-center'>soy la animacion</h1>
+            </div>
+        </div>
+            <div className='grid content-center w-full justify-items-center bg-cream-200'>
                 <p className='col-span-2'>© 2022 Copyright: FindBook.com</p>
             </div>
         </div>
--- src/components/smartComponents/ChatBot/ChatBot.jsx
+++ src/components/smartComponents/ChatBot/ChatBot.jsx
@@ -4,7 +4,7 @@ import messageParser from '../../smartComponents/ChatBot/MessageParser';
 import ActionProvider from '../../smartComponents/ChatBot/ActionProviderDocs';
 import config from '../../smartComponents/ChatBot/ChatBotConfig';
 import 'react-chatbot-kit/build/main.css';
-
+import imgs from '../../../assets/bot.png'
 function Chat() {
     const [showBot, toggleBot] = useState(false);
     const saveMessages = (messages, HTMLString) => {
@@ -36,10 +36,8 @@ function Chat() {
             )}
             <button onClick={() => {
                     toggleBot((prev) => !prev)
-                }} className = "flex flex-wrap items-center justify-center py-1 font-medium no-underline duration-500 rounded-full w-14 h-14 text-neutral-900 bg-cream-300 hover:text-white hover:border-solid hover:border-slate-50 hover:bg-cream-200">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" /*class="appChatbotButtonIcon_Lq7z"*/ className="rounded-full w-9 h-9">
-                    <path d="M192 408h64v-48h-64zm384-216h-32a96 96 0 00-96-96H344V24a24 24 0 00-48 0v72H192a96 96 0 00-96 96H64a48 48 0 00-48 48v128a48 48 0 0048 48h32a96 96 0 0096 96h256a96 96 0 0096-96h32a48 48 0 0048-48V240a48 48 0 00-48-48zM96 368H64V240h32zm400 48a48.14 48.14 0 01-48 48H192a48.14 48.14 0 01-48-48V192a48 48 0 0148-48h256a48 48 0 0148 48zm80-48h-32V240h32zM240 208a48 48 0 1048 48 47.996 47.996 0 00-48-48zm160 0a48 48 0 1048 48 47.996 47.996 0 00-48-48zm-16 200h64v-48h-64zm-96 0h64v-48h-64z"></path>
-                </svg>
+                }} className = "flex flex-wrap items-center justify-center py-1 mt-2 font-medium no-underline duration-500 rounded-full w-14 h-14 bg-slate-400 hover:text-white hover:border-solid hover:border-slate-50 hover:bg-cream-200">
+                <img src={imgs} className='h-10' alt='Not found'/>
             </button>
         </div>
     );
